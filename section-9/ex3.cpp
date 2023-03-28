a74a0fb2b45a644fb3ede44972bbedc3af8001a6
Gabriel Luculescu <gabe.luculescu@gmail.com>
2023-03-28 10:05:05 -0500

    Refactor logic to improve performance

--- section-9/ex3.cpp
+++ section-9/ex3.cpp
@@ -2,35 +2,33 @@
 #include <string>
 
 int main() {
-    
+
     std::string input, str;
     std::cin >> input >> str;
 
-    if(str.size() > input.size()) {
-        std::cout << "NO\n";
-    }
-    else {
-        size_t len{ input.size() - str.size() };
-        for(size_t i{}; i <= len; ++i) {
-            if(input[i] == str[0]) {
-                size_t k{};
-                size_t stop{ str.size() };
-                while(i != len && k != stop) {
-                     if(input[i] != str[k]) {
-                        std::cout << "NO\n";
-                        return 0;
-                     }
-                     ++i; ++k;
-                }
+     if(str.size() > input.size()) {
+         std::cout << "NO\n";
+         return 0;
+     }
 
-                std::cout << "YES\n";
-                return 0;
+     int str_len = int(str.size());
+     int input_len = int(input.size());
+     for(int i{}; i != input_len - str_len + 1; ++i) {
+         bool is_substring{true};
+        for(int j{i}, k{}; j != i + str_len; ++j, ++k) {
+            if(input[j] != str[k]) {
+                is_substring = false;
+                break;
             }
-            
         }
 
-        std::cout << "NO\n";
-    }
+        if(is_substring) {
+            std::cout << "YES\n";
+            return 0;
+        }
+     }
+
+     std::cout << "NO\n";
 
     return 0;
 }
